c95aee77f2372ee80d09747af0f54cba8d390ea5
Emilio Gonzalez Guerra <emiliogzz24@gmail.com>
2021-05-26 12:14:48 -0500

    Feature: wrapping up the menu

--- src/layouts/main/sidebar/index.jsx
+++ src/layouts/main/sidebar/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Form, Select, InputNumber, Divider, Button } from 'antd';
+import Text from 'antd/es/typography/Text';
+
 import { datasets } from '@config/constants';
 import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons';
 import { useVisualization } from '@providers/visualization';
 import Box from '@components/box';
 import { Sider } from './elements';
+import Graph from '../../../components/graph';
 
 const { Item } = Form;
 const { Option } = Select;
@@ -13,16 +16,27 @@ const Sidebar = () => {
   const {
     dataset,
     setDataset,
+    datasetData,
     classes,
     setClasses,
     samplesPerClass,
     setSamplesPerClass,
     layers,
     setLayers,
+    batchSize,
+    setBatchSize,
+    epoch,
+    setEpoch,
+    trainProportion,
+    setTrainProportion,
+    selectedPoint,
+    setSelectedPoint,
+    classification,
+    setClassification,
   } = useVisualization();
 
   return (
-    <Sider theme="light" collapsed={false}>
+    <Sider width={300} theme="light" collapsed={false}>
       <Divider orientation="left">Selecciona los datos</Divider>
       <Form
         layout="vertical"
@@ -41,6 +55,7 @@ const Sidebar = () => {
             ))}
           </Select>
         </Item>
+        <Graph dataset={datasetData} />
         <Item label="Clases" name="classes">
           <InputNumber
             min={0}
@@ -63,9 +78,21 @@ const Sidebar = () => {
         </Item>
       </Form>
       <Divider orientation="left">Entrena tu modelo</Divider>
-      <Form>
+      <Text strong>Define la cantidad de capas y número de neuronas por capa de tu modelo</Text>
+      <Form
+        initialValues={{
+          batchSize,
+          epoch,
+          trainProportion,
+        }}
+      >
         {layers.map((value, index) => (
-          <Item key={index} label={`Layer ${index + 1}`} name={`layer${index}`}>
+          <Item
+            style={{ marginTop: '10px' }}
+            key={index}
+            label={`Capa #${index + 1}`}
+            name={`layer${index}`}
+          >
             <Box display="flex" alignItems="center">
               <InputNumber
                 value={value}
@@ -77,6 +104,9 @@ const Sidebar = () => {
                     return newLayers;
                   })
                 }
+                formatter={(val) => {
+                  return `${val} neurona${val === '1' ? '' : 's'}`;
+                }}
                 style={{ width: '100%' }}
                 placeholder="Tamaño"
               />
@@ -95,7 +125,7 @@ const Sidebar = () => {
             </Box>
           </Item>
         ))}
-        <Item>
+        <Item style={{ marginTop: '10px' }}>
           <Button
             type="dashed"
             onClick={() =>
@@ -107,11 +137,61 @@ const Sidebar = () => {
             style={{ width: '100%' }}
             icon={<PlusOutlined />}
           >
-            Add field
+            Agregar capa
           </Button>
         </Item>
+        <Text strong>Configura las condiciones del entrenamiento de tu modelo</Text>
+        <Item style={{ marginTop: '10px' }} label="Tamaño del batch" name="batchSize">
+          <InputNumber min={0} onChange={setBatchSize} style={{ width: '100%' }} />
+        </Item>
+        <Item label="Epoch" name="epoch">
+          <InputNumber min={0} onChange={setEpoch} style={{ width: '100%' }} />
+        </Item>
+        <Item label="Proporción de entrenamiento" name="trainProportion">
+          <InputNumber
+            min={0}
+            step={0.1}
+            max={1}
+            onChange={setTrainProportion}
+            formatter={(val) => {
+              return `${val * 100}%`;
+            }}
+            style={{ width: '100%' }}
+          />
+        </Item>
       </Form>
+      <Text strong>Selecciona el punto que te gustaría clasificar con tu modelo entrenado</Text>
       <Divider orientation="left">Prueba tu modelo</Divider>
+      <Graph
+        dataset={datasetData}
+        selectedPoint={selectedPoint}
+        onClick={(event) => {
+          let valueX = null;
+          let valueY = null;
+          Object.keys(event.chart.scales).forEach((element) => {
+            const scale = event.chart.scales[element];
+            if (scale.isHorizontal()) {
+              valueX = scale.getValueForPixel(event.x);
+            } else {
+              valueY = scale.getValueForPixel(event.y);
+            }
+          });
+          setSelectedPoint({ x: valueX, y: valueY });
+        }}
+      />
+      <Button
+        type="primary"
+        onClick={() => {
+          console.log('Mandar a backend para predecir...');
+          setClassification(0);
+        }}
+        style={{ width: '100%' }}
+        icon={<PlusOutlined />}
+      >
+        Clasificar punto
+      </Button>
+      <Divider orientation="left">Resultados</Divider>
+      {classification !== null && <div>Mostrar acá la clasificación numérica + su color</div>}
     </Sider>
   );
 };
